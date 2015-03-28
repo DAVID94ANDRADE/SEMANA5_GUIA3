9dd9531bb5431f2a9d03e1b7a1952c6229533905
DAVID ANTONIO ANDRADE <david_andrade94@hotmail.com>
2015-03-28 03:10:21 +0100

    trabajo de guia 3 semana 5

---
+++ clases/SeccionControlador.php
@@ -0,0 +1,14 @@
+<?php
+include './clases/seccion.php';
+class SeccionControlador extends Secion{
+	public function guardarDatos($con,$objseccion){
+		$variableSql = "INSERT INTO  seccion";
+		$variableSql .= "(id,nombre,escuela)";
+		$variableSql .= "VALUES(";
+			$variableSql.="'".$objseccion[0]. "',";
+			$variableSql.="'".$objseccion[1]. "',";
+			$variableSql.="'".$objseccion[2]. "');";
+ return consultaA($con; $con, $variableSql);
+
+	}
+}
